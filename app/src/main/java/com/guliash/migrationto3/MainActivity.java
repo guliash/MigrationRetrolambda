91c622e87a3cdcbe9ca2c440fe8ed4d5434371ce
Artem Gilmudinov <guliash92@gmail.com>
2017-08-09 17:33:10 +0300

    minimal;

--- app/src/main/java/com/guliash/migrationto3/MainActivity.java
+++ app/src/main/java/com/guliash/migrationto3/MainActivity.java
@@ -1,10 +1,9 @@
 package com.guliash.migrationto3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.guliash.commons.ModelFromCommons;
-import com.guliash.models.ModelFromModels;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ModelFromModels.create(1, "2");
 
         ModelFromCommons.create(1, "2");
     }
